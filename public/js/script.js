52771a7b17bbb848bbb5b4cbf7b416f0a6872293
Brad Kriss <BradKriss@users.noreply.github.com>
2016-11-19 21:24:03 -0500

    Clicking outside box now closes box

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,15 @@
 $( document ).ready(function() {
+    
     $('.skill-box').click(function() {
       $(this).toggleClass('full-size').find('.skill-subtext').toggle();
-    })
+    });
+    
+    $(document).bind('click', function(e) {
+     console.log(e.target);
+      if(!$(e.target).parents('.skill-box').length && !$(e.target).hasClass('skill-box')) {
+        $('.skill-box').removeClass('full-size');
+        $('.skill-subtext').hide();
+      }
+    });
+    
 });
